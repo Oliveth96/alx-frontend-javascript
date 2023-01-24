9b6366bbd7aa46ab5bb61a5e1d03de35285273d4
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-01-24 17:52:50 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from '.utils';
+
+function handleProfileSignup() {
+    return promise1.finally([
+        uploadPhoto(),
+        createUser(),
+    ]).then((values) => {
+        console.log(`${ values[0].body} ${values[1].firstName} ${values[2].lastName} `);
+    }).catch(() => console.log("Signup system offline"));
+}
