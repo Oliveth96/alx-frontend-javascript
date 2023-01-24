3289fc567fe1ead9b6f2884a656bca25247b6412
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-01-24 18:24:12 +0100

    Returns a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,26 +1,25 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName,lastName,fileName) {
-    return Promise.allSettled([
-        signUpUser(fileName, lastName),
-        uploadPhoto(fileName)
-    ]).then((result) => {
-        const data = [];
-
-        result.forEach ((index) => {
-            if (index.status === 'fulfilled') {
-                data.push({
-                    status: index.status,
-                    value: index.value,
-                });
-            } else{
-                data.push({
-                    status: index.status,
-                    value: `Error: ${ index.reason.message} `,
-                });
-            }
-        });
-        return data;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)])
+    .then((result) => {
+      const array = [];
+      result.forEach((index) => {
+        if (index.status === 'fulfilled') {
+          array.push({
+            status: index.status,
+            value: index.value,
+          });
+        } else {
+          array.push({
+            status: index.status,
+            value: `Error: ${index.reason.message}`,
+          });
+        }
+      });
+      return array;
     });
 }
