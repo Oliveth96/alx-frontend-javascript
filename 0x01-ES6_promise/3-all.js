b47ab3c1a37b192e78f5e1894416ebfef90d9928
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-01-24 17:56:37 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from '.utils';
 
 export default function handleProfileSignup() {
-    return promise1.finally([
+    return Promise.all([
         uploadPhoto(),
         createUser(),
     ]).then((values) => {
