fd29b8d75eaaaab6bebfa42e18d67cd4564546e0
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-01-31 14:37:27 +0100

    A Function that returns a map of groceries with the following items (name, quantity)

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+const groceriesList = () => {
+  const defMap = new Map();
+  defMap.set("Apples", 10);
+  defMap.set("Tomatoes", 10);
+  defMap.set("Pasta", 1);
+  defMap.set("Rice", 1);
+  defMap.set("Banana", 5);
+  return defMap;
+};
+
+export default groceriesList;
