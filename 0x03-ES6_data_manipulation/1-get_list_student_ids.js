ba458d73a6c64fa4369036727ab5a160269c934a
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-01-31 13:38:19 +0100

    A function getListStudentIds that returns an array of ids from a list of object

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 const getListStudentIds = (getList) => {
-    let listData = [];
-    if (!(getList instanceof Array)) {
-      return listData;
-    }
-    listData = getList.map((results) => results.id);
+  let listData = [];
+  if (!(getList instanceof Array)) {
     return listData;
-  };
-  export default getListStudentIds;
+  }
+  listData = getList.map((results) => results.id);
+  return listData;
+};
+export default getListStudentIds;
